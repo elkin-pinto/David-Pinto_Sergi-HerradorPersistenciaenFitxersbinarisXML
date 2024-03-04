ab59ddb396ed2a6019826d8cb1c7be7ae6c1b726
elkin-pinto <elkin.pinto.7e7@itb.cat>
2024-03-04 11:31:03 +0100

    commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,10 @@
+package org.example
+
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import java.io.File
+
+fun main() {
+    println("Hello World!")
+}
